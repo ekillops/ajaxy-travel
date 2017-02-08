3df8d84e5657267ff1ca4d56f5600410b2ef6c0a
Brad Copenhaver and Ryan Peterson <brad+ryan@>
2017-02-08 14:50:33 -0800

    add create and edit functionality for experiences

--- src/TravelBlogAgain/Controllers/ExperiencesController.cs
+++ src/TravelBlogAgain/Controllers/ExperiencesController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TravelBlogAgain.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -23,6 +24,36 @@ namespace TravelBlogAgain.Controllers
                 .ToList();
             ViewBag.Persons = persons;
             return View(thisExperience);
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.LocationId = new SelectList(db.Locations, "LocationId", "Name");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Experience experience)
+        {
+            db.Experiences.Add(experience);
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "Locations");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisExperience = db.Experiences.FirstOrDefault(experiences => experiences.ExpId == id);
+            ViewBag.LocationId = new SelectList(db.Locations, "LocationId", "Name");
+            return View(thisExperience);
+
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Experience experience)
+        {
+            db.Entry(experience).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Details", new { id = experience.ExpId });
         }
     }
 }
